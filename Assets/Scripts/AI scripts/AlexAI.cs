15ce2a36590d6168ecead68c599bec0f2c0b963d
ShadowLordAlex <alexanderschuur@gmail.com>
2020-04-25 01:31:41 +0200

    Fixed up the screen order, Made a test level and MANUAL controls for the ship, Made Naming for the ship and spawning it in, Made a health bar

---
+++ Assets/Scripts/AI scripts/AlexAI.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AlexAI : BaseAI
+{
+    public override IEnumerator RunAI()
+    {
+        while (true)
+        {
+            yield return Ahead(300);
+            yield return TurnRight(180);
+        }
+    }
+
+    public override void OnScannedRobot(ScannedRobotEvent e)
+    {
+    }
+}
