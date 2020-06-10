b7b2cefc5f76f7e2c1420825a125d0acd5b73e39
Sean <yangxiao1998@gmail.com>
2020-06-11 00:36:08 +0200

    Upgrade my AI (WIP)
    Fixed aiming issue, transfer the v3 into v2 to calculate cross, in order to check target is on left or right

--- Assets/Scripts/AI scripts/SeanAI.cs
+++ Assets/Scripts/AI scripts/SeanAI.cs
@@ -4,40 +4,56 @@ using UnityEngine;
 
 public class SeanAI : BaseAI
 {
-    private int RandomNumber = 0;
     public override IEnumerator RunAI()
     {
-        for(int i = 0; i < 100; i++)
+        for (int i = 0; i < 100; i++)
         {
+        fire:
             if (TargetsContain(out Vector3 position, GameItems.Enemy))
             {
                 Vector3 lko = (Ship.transform.position - Ship.GetComponentInChildren<LookoutCollider>().gameObject.transform.position).normalized;
                 Vector3 tar = (Ship.transform.position - position).normalized;
                 var angle = Vector3.Angle(lko, tar);
-                Debug.Log(angle);
-                Debug.Log(Vector3.Cross(lko, tar).z);
-                if (Vector3.Cross(lko, tar).z < 0)
+                Vector2 lkoV2 = new Vector2(lko.x, lko.z);
+                Vector2 tarV2 = new Vector2(tar.x, tar.z);
+                Debug.Log(Vector3.Cross(lkoV2, tarV2).z);
+                if (Vector3.Cross(lkoV2, tarV2).z > 0)
                 {
                     yield return TurnLeft(angle);
-                    yield return TurnLookoutLeft(angle);
+                    yield return TurnLookoutLeft(angle / 2);
                     yield return FireFront(1000);
                     yield return Ahead(10);
                 }
-                else if(Vector3.Cross(lko, tar).z > 0)
+                else if (Vector3.Cross(lkoV2, tarV2).z < 0)
                 {
                     yield return TurnRight(angle);
-                    yield return TurnLookoutRight(angle);
+                    yield return TurnLookoutRight(angle / 2);
                     yield return FireFront(1000);
                     yield return Ahead(10);
                 }
                 else
                 {
+                    yield return FireFront(1000);
                     yield return Ahead(10);
                 }
             }
             else
-            {
-                yield return Ahead(25);
+                {
+                for(int j = 0; j < 7; j ++)
+                {
+                    yield return Ahead(50);
+                    if (TargetsContain(out _, GameItems.Enemy))
+                        goto fire;
+                }
+                var rnd = Random.Range(-1, 1);
+                if(rnd > 0)
+                {
+                    yield return TurnLeft(90);
+                }
+                else
+                {
+                    yield return TurnRight(90);
+                }
             }
         }
     }
